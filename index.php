006113a8f8873798fb380779381e38ba3f98a9e5
Robert Bunch <rob@digitalcrafts.com>
2016-05-18 12:00:25 -0400

    Add textarea for posts, add posts to index

--- index.php
+++ index.php
@@ -6,11 +6,38 @@
 
 	require_once 'includes/head.php';
 	require_once 'includes/header.php';
+
+	$posts = DB::query("SELECT * FROM posts");
 ?>
 
-
-
 <body>
-	MantaRay site
+
+	<h1>Make a post</h1>
+	<?php if(isset($_SESSION['username'])): ?>
+		<form action="post_process.php" method="post">
+		  <div class="form-group">
+		    <label for="post-text">Your Manta Ray post</label>
+			<textarea id="post-text" name="post_text"></textarea>
+		  </div>
+		  <button type="submit" class="btn btn-default">Post</button>
+		</form>
+	<?php else: ?>
+		<h3>You must be <a href="login.php">logged in</a> to make a post</h3>
+	<?php endif; ?>
+	
+	<?php foreach($posts as $post): ?>
+		<div class="post">
+			<div class="user"><?php print $post['username']; ?></div>
+			<div class="text"><?php print $post['postText']; ?></div>
+			<div class="date"><?php print $post['timestamp']; ?></div>
+		</div>
+		
+	<?php endforeach; ?>
+
 </body>
 </html>
+
+
+
+
+
---
+++ post_process.php
@@ -0,0 +1,30 @@
+<?php
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	session_start();
+	if(!isset($_SESSION['username'])){
+		//you aren't logged in. You should not be here. Goodbye.
+		header('Location: login.php');
+		exit;
+	}
+	require_once 'includes/meekrodb.2.3.class.php';
+	DB::$user = 'x';
+	DB::$password = 'x';
+	DB::$dbName = 'mantaray';
+	DB::$host = '127.0.0.1';
+
+	DB::insert('posts', array(
+		'username' => $_SESSION['username'],
+		'postText' => $_POST['post_text']
+	));
+	header('Location: index.php?post=success');
+
+
+
+
+
+
+
