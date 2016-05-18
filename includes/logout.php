fbb99f3e5f6b4ab5ad8e5c1238c61e6ac0245159
Robert Bunch <rob@digitalcrafts.com>
2016-05-18 10:08:35 -0400

    Remove rogue logout file

--- includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_destroy();
-require_once 'includes/head.php';
-require_once 'includes/header.php';
-
-	print '<h2>Thank you for your support of the Manta Ray</h2>';
-
-require_once 'includes/footer.php';
